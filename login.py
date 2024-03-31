b2e81e2f7ecfd5bf9e95a4e1c2df7222e0c3f3e1
Heinrich Mises <henrique.lapf@gmail.com>
2024-03-31 20:27:06 -0300

    add option select in while loop

--- login.py
+++ login.py
@@ -1,6 +1,16 @@
 
+while True:
+    print('Escolha uma opção:')
+    print('1) Login')
+    print('2) Cadastro ')
+    print('3) Sair ')
+    opcao = int(input())
 
-base = int(input("Digite a base "))
-altura = int(input("Digite a altura: "))
-
-print("A hipotenusa é:", pow(pow(base,2)+pow(altura,2),0.5) )
+    if(opcao == 1):
+        print('opcao de login')
+    elif(opcao == 2):
+        print('opcao de cadastro')
+    elif(opcao == 3):
+        print('bye')
+        break
+    else: print('opção inválida, tente novamente')
