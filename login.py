c0dd592624097d21261925c7854b3596b3fca9bd
Heinrich Mises <henrique.lapf@gmail.com>
2024-03-31 18:48:48 -0300

    voltando

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 
 
-# base = int(input("Digite a base "))
-# altura = int(input("Digite a altura: "))
-print("A hipotenusa é:", pow(pow(int(input("Digite a altura: ")),2)+pow(int(input("Digite a base ")),2),0.5) )
+base = int(input("Digite a base "))
+altura = int(input("Digite a altura: "))
 
+print("A hipotenusa é:", pow(pow(base,2)+pow(altura,2),0.5) )
