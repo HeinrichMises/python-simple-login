849f56d9c4fedac3564c12bd0613f63c40ce9412
Heinrich Mises <henrique.lapf@gmail.com>
2024-04-01 00:07:16 -0300

    add register function

--- login.py
+++ login.py
@@ -1,3 +1,7 @@
+def cadastrar_usuario(usuario, senha):
+    with open("contas.txt", "a") as file:
+        file.write(f"{usuario}:{senha}\n")
+
 def confirmar_senha(senha, confirmacao_senha):
     while senha != confirmacao_senha:
         print('senhas não conferem')
@@ -5,13 +9,16 @@ def confirmar_senha(senha, confirmacao_senha):
         confirmacao_senha = input('Confirme a senha: ')
     return senha
 
-def confirmar_usuario(usuario, senha):
-    with open("contas.txt") as contas:
+def confirmar_login(usuario, senha):
+    with open("contas.txt", "r") as contas:
         for linha in contas:
             user, passwd = linha.strip().split(":")
             if user == usuario and passwd == senha:
                 return True
     return False
+
+
+
 
 while True:
     print('Escolha uma opção:')
@@ -24,14 +31,19 @@ while True:
         print('opcao de login')
         usuario = input('Digite o nome de usuário: ')
         senha = input('Digite a senha: ')
-        if confirmar_usuario(usuario, senha): print('login realizado com sucesso!')
+        if confirmar_login(usuario, senha): print('login realizado com sucesso!')
         
-    
     elif(opcao == 2):
         usuario = input('Digite o nome de usuário: ')
         senha = input('Digite a senha: ')
         confirmacao_senha = input('Confirme a senha: ')
+        
         senha = confirmar_senha(senha, confirmacao_senha)
+        cadastrar_usuario(usuario, senha)
+        print("usuario cadastrado com sucesso")
+        
+
+
         
     elif(opcao == 3):
         print('bye')
