8b905d4562ba696ed57252b192598a6a60f7bfa7
Heinrich Mises <henrique.lapf@gmail.com>
2024-03-31 21:47:21 -0300

    fix password confirmation function

--- login.py
+++ login.py
@@ -1,28 +1,26 @@
-
 def confirmar_senha(senha, confirmacao_senha):
-    if senha != confirmacao_senha:
+    while senha != confirmacao_senha:
         print('senhas não conferem')
-        senha = input('Digite a senha novamente')
-        confirmacao_senha = input('Confirme a senha')
-        confirmar_senha(senha, confirmacao_senha)
-    return
+        senha = input('Digite a senha novamente: ')
+        confirmacao_senha = input('Confirme a senha: ')
+    return senha
 
 while True:
     print('Escolha uma opção:')
     print('1) Login')
-    print('2) Cadastro ')
-    print('3) Sair ')
-    opcao = int(input('>'))
+    print('2) Cadastro')
+    print('3) Sair')
+    opcao = int(input('> '))
 
     if(opcao == 1):
-        usuario = input('Digite o nome de usuário')
-        senha = input('Digite a senha')
-        confirmacao_senha = input('Confirme a senha')
-        confirmar_senha(senha, confirmacao_senha)
-
-
+        print('opcao de login')
+    
     elif(opcao == 2):
-        print('opcao de cadastro')
+        usuario = input('Digite o nome de usuário: ')
+        senha = input('Digite a senha: ')
+        confirmacao_senha = input('Confirme a senha: ')
+        senha = confirmar_senha(senha, confirmacao_senha)
+        
     elif(opcao == 3):
         print('bye')
         break
